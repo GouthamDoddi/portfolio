80061b41627c4a75a11649c362656523c1570f9b
Goutham-zern <goutham@zern.ai>
2024-04-05 17:46:41 +0530

    add about-us and department

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/3d_portfolio/',
   plugins: [react()],
   assetsInclude: ['**/*.glb']
 })
