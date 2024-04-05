783b69c2ebb63e7e5da0f9fb581c34e376aa69ac
Goutham-zern <goutham@zern.ai>
2024-04-05 17:08:07 +0530

    added gh-pages and congif changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/3d_portfolio/',
   plugins: [react()],
   assetsInclude: ['**/*.glb']
 })
